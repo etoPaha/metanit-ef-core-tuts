4b97f0a01af05901ff0f74d0f44dc87fec712b90
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2023-04-10 10:17:24 +1000

    Добавил пример с использованием appsettings.json

--- n4-db-connection-conf/Program.cs
+++ n4-db-connection-conf/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace n4_db_connection_conf
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            UseAppsettingsFile_Example();
+            
+            Console.ReadLine();
+        }
+
+        private static void DbContextOptions_InConstructor_Example()
         {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
 
@@ -21,8 +30,51 @@ namespace n4_db_connection_conf
                 foreach (User u in users)
                     Console.WriteLine($"{u.Id} {u.Name} - {u.Age}");
             }
+        }
+
+        private static void UseAppsettingsFile_Example()
+        {
+            var builder = new ConfigurationBuilder();
+            // Установка пути к текущему каталогу
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            // получаем конфигурацию из файла appsettings.json
+            builder.AddJsonFile("appsettings.json");
+            // создаем конфигурацию
+            var config = builder.Build();
+            // получаем строку подключения
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            var options = optionsBuilder
+                .UseSqlServer(connectionString)
+                .Options;
             
-            Console.ReadLine();
+            AddUsers(options);
+
+            using (ApplicationContext db = new ApplicationContext(options))
+            {
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id} {u.Name} - {u.Age}");
+                }
+            }
+        }
+
+        private static void AddUsers(DbContextOptions<ApplicationContext> options)
+        {
+            using (ApplicationContext db = new ApplicationContext(options))
+            {
+                User user1 = new User { Name = "Max", Age = 5 };
+                User user2 = new User { Name = "Vika", Age = 31 };
+                User user3 = new User { Name = "Pasha", Age = 32 };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.Users.Add(user3);
+
+                db.SaveChanges();
+            }
         }
     }
 }
