2b4cf7000e40606b90f38019b8fd4aac4bdea5a2
Pavel Berezkin <cheliabkns@gmail.com>
2023-04-10 22:44:41 +1000

    Простой пример логирования операций

---
+++ n5-log-operations/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace n5_log_operations
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLExpress;Database=helloappdb;Trusted_Connection=true;");
+            optionsBuilder.LogTo(Console.WriteLine);
+        }
+    }
+}
---
+++ n5-log-operations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace n5_log_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+
+                var users = db.Users.ToList();
+
+                Console.WriteLine("Данные после добавления:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id} {u.Name} - {u.Age}");
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
